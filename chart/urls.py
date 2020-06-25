c348778338ab6ab3541f5ecb525a792e19f1bff1
yunsun1127 <yunsun1127@naver.com>
2020-06-25 15:21:32 +0900

    Covid19

--- chart/urls.py
+++ chart/urls.py
@@ -6,8 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('ticket-class/1/',
          views.ticket_class_view_1, name='ticket_class_view_1'),
-    path('json-example/', views.json_example, name='json_example'),
-    path('json-example/data/', views.chart_data, name='chart_data'),
     path('covid/', views.covid, name='covid'),
     path('covid19/', views.covid19, name='covid19'),
     path('admin/', admin.site.urls),
--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&b=&%(3wiq&+25srs%i$^*d5i89lp%l#+&)02b4976nllto*u2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
