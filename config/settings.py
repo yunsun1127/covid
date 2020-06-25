4e754954bf50c379d59490b72d7dff5f522e104c
yunsun1127 <yunsun1127@naver.com>
2020-06-25 16:39:42 +0900

    add allowed_hosts

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&b=&%(3wiq&+25srs%i$^*d5i89lp%l#+&)02b4976nllto*u2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1:8000',
